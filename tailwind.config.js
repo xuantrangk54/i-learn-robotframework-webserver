bed18abf9d5b0bf2c3af8351109126165e6172bb
unknown <xuantrangk54@gmail.com>
2025-10-19 11:14:15 +0700

    chane

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
